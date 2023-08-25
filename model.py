32992650ca4443349759ca520ebc50efca1a7e8b
BrightProgrammer7 <abdelilahk627@gmail.com>
2023-08-25 19:51:11 +0100

    first commit

--- model.py
+++ model.py
@@ -14,12 +14,14 @@ X = df[["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]]
 y = df["Class"]
 
 # Split the dataset into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=50)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=50
+)
 
 # Feature scaling
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test= sc.transform(X_test)
+X_test = sc.transform(X_test)
 
 # Instantiate the model
 classifier = RandomForestClassifier()
